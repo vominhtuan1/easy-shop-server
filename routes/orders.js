843df97736ef030b2b6b549a74f050266bc7d1d1
vominhtuan1 <19522487@gm.uit.edu.vn>
2021-04-13 12:03:40 +0700

    fourth commit

--- routes/orders.js
+++ routes/orders.js
@@ -68,19 +68,18 @@ router.post(`/`, async (req,res)=>{
 })
 
 
-router.put(`/:id`, async (req, res) => {
+router.put('/:id',async (req, res)=> {
     const order = await Order.findByIdAndUpdate(
         req.params.id,
         {
             status: req.body.status
-        },{
-            new: true
-        }
+        },
+        { new: true}
     )
 
-    if(!order) {
-        return res.status(400).send('the order cannot be updated')
-    } 
+    if(!order)
+    return res.status(400).send('the order cannot be update!')
+
     res.send(order);
 })
 
