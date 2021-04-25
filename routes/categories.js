c51377446e3e25b3f2eed88a0be8409dfd5a0b3e
vominhtuan1 <19522487@gm.uit.edu.vn>
2021-04-25 14:17:38 +0700

    commit

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,33 @@ const {Category} = require('../models/category');
 const express = require('express');
 const { route } = require('./products');
 const router = express.Router();
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if (isValid) {
+            uploadError = null;
+        }
+        cb(uploadError, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
+    },
+});
+
+const uploadOptions = multer({ storage: storage });
+
 
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
@@ -12,21 +39,37 @@ router.get(`/`, async (req, res) =>{
     res.send(categoryList);
 })
 
-router.put(`/:id`, async (req, res) => {
-    const category = await Category.findByIdAndUpdate(
+router.put(`/:id`,uploadOptions.single('image'),async (req, res) => {
+
+    const category = await Category.findById(req.params.id);
+    if (!category) return res.status(400).send('Invalid Category!');
+
+    const file = req.file;
+    let iconpath;
+
+    if (file) {
+        const fileName = file.filename;
+        const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+        iconpath = `${basePath}${fileName}`;
+    } else {
+        iconpath = category.icon;
+    }
+
+
+    const updateCategory = await Category.findByIdAndUpdate(
         req.params.id,
         {
             name: req.body.name,
-            icon: req.body.icon
+            icon: iconpath
         },{
             new: true
         }
     )
 
-    if(!category) {
+    if(!updateCategory) {
         return res.status(400).send('the category cannot be updated')
     } 
-    res.send(category);
+    res.send(updateCategory);
 })
 
 router.get('/:id', async(req,res)=>{
@@ -38,10 +81,16 @@ router.get('/:id', async(req,res)=>{
     res.status(200).send(category);
 })
 
-router.post(`/`, async (req, res) =>{
+router.post(`/`,uploadOptions.single('image'), async (req, res) =>{
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
+
+    const fileName = file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
     let category = new Category({
         name: req.body.name,
-        icon: req.body.icon
+        icon: `${basePath}${fileName}`
     })
 
     category = await category.save();
